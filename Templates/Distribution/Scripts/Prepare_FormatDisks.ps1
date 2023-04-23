0d1d1ad3688632f0ee9b51566d08cadafca165bb
Nick Young <mrbiggbrain@gmail.com>
2023-04-23 00:02:40 -0400

    Work on disk formatting script

--- Templates/Distribution/Scripts/Prepare_FormatDisks.ps1
+++ Templates/Distribution/Scripts/Prepare_FormatDisks.ps1
@@ -53,3 +53,38 @@ if($ChosenDisk.OperationalStatus -ne "Online")
     [Logger]::WriteError($err)
     [ProgramScope]::FailProvision($err)
 }
+
+# -----------------------------------------------------------
+# Clear Disk
+# -----------------------------------------------------------
+clear-disk -number $ChosenDisk.DiskNumber -removedata
+
+# -----------------------------------------------------------
+# Partition Disk
+# -----------------------------------------------------------
+
+# Recovery
+$RecoveryPartition = New-Partition -DiskNumber $ChosenDisk.DiskNumber -Size 499MB -DriveLetter R -GptType "{de94bba4-06d1-4d40-a16a-bfd50179d6ac}"
+
+# EFI
+$BootPartition = New-Partition -DiskNumber $ChosenDisk.DiskNumber -Size 499MB -DriveLetter E  -GptType "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}"
+
+# Reserved
+$ReservedPartition = New-Partition -DiskNumber $ChosenDisk.DiskNumber -Size 16MB -DriveLetter M -GptType "{e3c9e316-0b5c-4db8-817d-f92df00215ae}"
+
+# OS
+$OSPartition = New-Partition -DiskNumber $ChosenDisk.DiskNumber -DriveLetter C -UseMaximumSize
+
+
+# -----------------------------------------------------------
+# Format Disks
+# -----------------------------------------------------------
+
+# Recovery 
+Format-Volume –DriveLetter R -FileSystem NTFS -FileSystemLabel "Recovery"
+
+# Boot
+Format-Volume –DriveLetter E -FileSystem FAT32 -FileSystemLabel "Boot"
+
+# OS
+Format-Volume –DriveLetter C -FileSystem NTFS -FileSystemLabel "OS"
